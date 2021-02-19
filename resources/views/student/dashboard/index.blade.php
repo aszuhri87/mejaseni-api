39627320cb71f9fc89b3ba74de2c52bb75dd7fef
Yudha Prasetyo <yprasetyo72@gmail.com>
2021-02-19 18:45:26 +0700

    dashboard

--- resources/views/student/dashboard/index.blade.php
+++ resources/views/student/dashboard/index.blade.php
@@ -1,5 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="row" style="margin-left: 0 !important">
+        <div class="col-lg-8" style="padding-left: 0 !important;">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Summary Course</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <!--begin::Mixed Widget 14-->
+            <div class="card card-custom card-stretch gutter-b">
+                <!--begin::Header-->
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title font-weight-bolder">Action Needed</h3>
+                    <div class="card-toolbar">
+                        <div class="dropdown dropdown-inline">
+                            <a href="#" class="btn btn-clean btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ki ki-bold-more-hor"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+                                <!--begin::Navigation-->
+                                <ul class="navi navi-hover py-5">
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-drop"></i>
+                                            </span>
+                                            <span class="navi-text">New Group</span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-list-3"></i>
+                                            </span>
+                                            <span class="navi-text">Contacts</span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-rocket-1"></i>
+                                            </span>
+                                            <span class="navi-text">Groups</span>
+                                            <span class="navi-link-badge">
+                                                <span class="label label-light-primary label-inline font-weight-bold">new</span>
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-bell-2"></i>
+                                            </span>
+                                            <span class="navi-text">Calls</span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-gear"></i>
+                                            </span>
+                                            <span class="navi-text">Settings</span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-separator my-3"></li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-magnifier-tool"></i>
+                                            </span>
+                                            <span class="navi-text">Help</span>
+                                        </a>
+                                    </li>
+                                    <li class="navi-item">
+                                        <a href="#" class="navi-link">
+                                            <span class="navi-icon">
+                                                <i class="flaticon2-bell-2"></i>
+                                            </span>
+                                            <span class="navi-text">Privacy</span>
+                                            <span class="navi-link-badge">
+                                                <span class="label label-light-danger label-rounded font-weight-bold">5</span>
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <!--end::Navigation-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end::Header-->
+                <!--begin::Body-->
+                <div class="card-body d-flex flex-column">
+                    <div class="flex-grow-1" style="position: relative;">
+                        <div id="progress-class"></div>
+                    <div class="resize-triggers"><div class="expand-trigger"><div style="width: 321px; height: 252px;"></div></div><div class="contract-trigger"></div></div></div>
+                    <div class="pt-5">
+                        <p class="text-center">46% sesi dari 2 kelas reguler aktif yang Anda miliki telah Anda hadiri.</p>
+                        <a href="#" class="btn btn-success btn-shadow-hover font-weight-bolder w-100 py-3">Generate Report</a>
+                    </div>
+                </div>
+                <!--end::Body-->
+            </div>
+            <!--end::Mixed Widget 14-->
+        </div>
+    </div>
 @endsection
+
+@push('script')
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    @include('student.dashboard.script')
+@endpush
