344e7fb69c8b5261534706b9993fcf767529d4c6
Muhammad Taufik Hidayat <mtaufiikh@gmail.com>
2021-03-08 15:54:09 +0700

    update migration

--- database/migrations/2021_03_08_155205_update_news_table_rev1.php
+++ database/migrations/2021_03_08_155205_update_news_table_rev1.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateTableNews extends Migration
+class UpdateNewsTableRev1 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,11 @@ class UpdateTableNews extends Migration
     public function up()
     {
         Schema::table('news', function (Blueprint $table) {
-            $table->uuid('classroom_category_id');
+            $table->dropColumn('classroom_category_id');
+        });
+
+        Schema::table('news', function (Blueprint $table) {
+            $table->uuid('classroom_category_id')->nullable();
             $table->foreign('classroom_category_id')->references('id')->on('classroom_categories');
         });
     }
@@ -28,6 +32,11 @@ class UpdateTableNews extends Migration
     {
         Schema::table('news', function (Blueprint $table) {
             $table->dropColumn('classroom_category_id');
+        });
+
+        Schema::table('news', function (Blueprint $table) {
+            $table->uuid('classroom_category_id')->nullable();
+            $table->foreign('classroom_category_id')->references('id')->on('classroom_categories');
         });
     }
 }
