4a502ca48d5231a3fe0d0e613ae5c2b4f5e8a08c
Muhammad Taufik Hidayat <mtaufiikh@gmail.com>
2021-03-08 13:40:07 +0700

    add bank account

---
+++ app/Models/BankAccount.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Uuid;
+
+class BankAccount extends Model
+{
+    use HasFactory, Uuid, SoftDeletes;
+
+    public $incrementing = false;
+
+    public $keyType = 'string';
+
+    protected $fillable = [
+        'coach_id',
+        'bank',
+        'bank_number',
+        'name_account'
+    ];
+
+    protected $dates = ['deleted_at'];
+}
---
+++ database/migrations/2021_03_08_133553_create_bank_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('coach_id');
+            $table->string('bank');
+            $table->string('bank_number');
+            $table->string('name_account');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('coach_id')->references('id')->on('coaches');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
