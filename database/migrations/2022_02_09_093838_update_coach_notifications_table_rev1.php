41703bb2e9fc71774b1ed6c67473e2f395062be3
Achmad Saefuddin Zuhri <achmad.s.zuhri182@gmail.com>
2022-04-05 14:50:13 +0700

    new migration v4

--- database/migrations/2022_02_09_093838_update_coach_notifications_table_rev1.php
+++ database/migrations/2022_02_09_093838_update_coach_notifications_table_rev1.php
@@ -14,9 +14,16 @@ class UpdateCoachNotificationsTableRev1 extends Migration
     public function up()
     {
         Schema::table('coach_notifications', function (Blueprint $table) {
+            $table->uuid('income_transaction_id')->nullable();
             $table->string('text_title')->nullable();
             $table->string('to_page')->nullable();
             $table->string('icon_url')->nullable();
+
+            $table->foreign('income_transaction_id')
+            ->references('id')
+            ->on('income_transactions')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
         });
     }
 
@@ -28,6 +35,7 @@ class UpdateCoachNotificationsTableRev1 extends Migration
     public function down()
     {
         Schema::table('coach_notifications', function (Blueprint $table) {
+            $table->dropColumn('income_transaction_id');
             $table->dropColumn('text_title');
             $table->dropColumn('to_page');
             $table->dropColumn('icon_url');
